203f08aef6e5f9cd73e4cdd7d77a0bb95dea30e5
William Lu <william.a.lu@gmail.com>
2015-03-03 23:14:28 -0500

    added code that parses through csv file of historical stock data and sentiment

---
+++ initial_vis.py
@@ -0,0 +1,14 @@
+import csv
+
+def initial_vis():
+	with open('sentdex_section.csv') as csvfile:
+		reader = csv.DictReader(csvfile)
+
+		for row in reader:
+			print row
+		# 	print(row['id'], row['time'], row['type'], row['value'],
+		# 		row['open'], row['close'], row['high'], row['low'],
+		# 		row['MA100'], row['MA250'], row['MA500'], row['M5000'])
+
+if __name__ == '__main__':
+	initial_vis()
