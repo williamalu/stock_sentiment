b44c7a676e71c37632db7e50ab5bdc0c008e561e
William Lu <william.a.lu@gmail.com>
2015-03-11 21:37:03 -0400

    processing sentiment working, test code present in plot_candlestick

--- candlestick_plot.py
+++ candlestick_plot.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import time
 import pandas as pd
 import ystockquote
 
@@ -8,13 +9,21 @@ from bokeh.plotting import *
 from bokeh.models import HoverTool 
 from collections import OrderedDict
 
-def convert_time(unix_time):
-	"""
-	Converts unix time to 'normal' time
-	returns date from unix time
-	Note: not currently being used
-	"""
-	return(datetime.datetime.fromtimestamp(int(unix_time)).strftime('%Y-%m-%d'))
+class convert_time(object):
+	"""Converts time back and forth between unix time and normal time"""
+	def __init__(self, time):
+		self.time = time
+
+	def __str__(self):
+		return self.time
+
+	def convert_to_ISO(self):
+		return datetime.datetime.fromtimestamp(int(self.time)).strftime('%Y-%m-%d')
+
+	def convert_to_datetime(self):
+		"""Converts ISO date in string format to datetime format"""
+		datetime_conv = time.strptime(str(self), "%Y-%m-%d")
+		return datetime.date(datetime_conv.tm_year, datetime_conv.tm_mon, datetime_conv.tm_mday)
 
 def load_stock(ticker,start_date,end_date):
 	"""
@@ -43,6 +52,57 @@ def load_stock(ticker,start_date,end_date):
 
 	return data
 
+def load_sentiment(ticker,start_date,end_date):
+	"""
+	Loads sentiment from sentdex csv file as a dictionary 
+	with format {date1:[1,2,3...] , date2:[1,2,3...]}
+	"""
+	data = {}
+
+	with open('stocks_sentdex.csv') as f:
+		next(f)
+		fieldnames = ['row_id','time','ticker','value','open_price','close','high','low','MA100','MA250','MA500','M5000']
+		reader = csv.DictReader(f, fieldnames = fieldnames)
+
+		time_step = datetime.timedelta(days = 1)
+		curr_date = start_date
+		curr_date = convert_time(curr_date)
+		curr_date = curr_date.convert_to_datetime() #converts curr_date to datetime format for incrementing
+		end_date = convert_time(end_date)
+		end_date = end_date.convert_to_datetime() #converts end_date to datetime format for incrementing
+		date_list = []
+		while curr_date <= end_date:
+			date_list.append(str(curr_date))
+			curr_date += time_step
+
+		for row in reader:
+			converted_time = convert_time(row['time'])
+			for a in date_list:
+				if a == converted_time.convert_to_ISO():
+					if row['ticker'] == ticker.lower():
+						if converted_time.convert_to_ISO() not in data:
+							data[converted_time.convert_to_ISO()] = list()
+							data[converted_time.convert_to_ISO()].append(int(row['value']))
+						else:
+							data[converted_time.convert_to_ISO()].append(int(row['value']))
+					else:
+						pass #ignores row of data if the data is not for a ticker we care about
+				else:
+					pass
+
+	return data
+
+def process_sentiment(data):
+	"""
+	Averages all the sentiment numbers for a date key in a dictionary
+	"""
+	avg_data = {}
+	for k in data:
+		avg_sent = sum(data[k]) / float(len(data[k]))
+		avg_data[k] = avg_sent
+
+	return avg_data
+
 def plot_candlestick():
 
 	ticker = raw_input('What is the stock ticker you want to graph? ')
@@ -50,6 +110,14 @@ def plot_candlestick():
 	end_date = raw_input('What end date would you like? [yyyy-mm-dd] ')
 
 	data = load_stock(ticker, start_date, end_date)
+
+	"""Test code"""
+	sent_data = load_sentiment(ticker, start_date, end_date)
+	print sent_data
+	avg_data = process_sentiment(sent_data)
+	print avg_data
+	"""End test code"""
+
 	df = pd.DataFrame(data)
 	df["date"] = pd.to_datetime(df["date"])
 
