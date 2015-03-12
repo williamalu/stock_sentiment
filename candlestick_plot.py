a7d474c44f05035f37128a9cdea02ebbd9bdd0e1
William Lu <william.a.lu@gmail.com>
2015-03-12 03:06:43 -0400

    added sentiment data to tooltips

--- candlestick_plot.py
+++ candlestick_plot.py
@@ -1,3 +1,15 @@
+"""
+A Python program that pulls stock price and sentiment data from 
+Yahoo Finance and SentDex, respectively, and graphs an interactive
+candlestick chart.
+
+To use: install ystockquote and bokeh
+
+On docstrings: this code is not "doctestable" because almost all our
+data is stored in dictionaries.  Keys are not stored in the same order
+on every computer, which means doctests will not work properly.
+"""
+
 import csv
 import datetime
 import time
@@ -9,6 +21,10 @@ from bokeh.plotting import *
 from bokeh.models import HoverTool 
 from collections import OrderedDict
 
+"""
+on docstrings: this code is not doctestable for two main reasons
+"""
+
 class convert_time(object):
 	"""Converts time back and forth between unix time and normal time"""
 	def __init__(self, time):
@@ -18,11 +34,16 @@ class convert_time(object):
 		return self.time
 
 	def convert_to_ISO(self):
+		"""
+		Converts from unix timestamps to ISO time (string output)
+		"""
 		return datetime.datetime.fromtimestamp(int(self.time)).strftime('%Y-%m-%d')
 
 	def convert_to_datetime(self):
-		"""Converts ISO date in string format to datetime format"""
-		datetime_conv = time.strptime(str(self), "%Y-%m-%d")
+		"""
+		Converts ISO date in string format to datetime format
+		"""
+		datetime_conv = time.strptime(str(self), "%Y-%m-%d") #separates out each component of ISO date
 		return datetime.date(datetime_conv.tm_year, datetime_conv.tm_mon, datetime_conv.tm_mday)
 
 def load_stock(ticker,start_date,end_date):
@@ -96,10 +117,15 @@ def process_sentiment(data):
 	"""
 	Averages all the sentiment numbers for a date key in a dictionary
 	"""
-	avg_data = {}
+	avg_data = {
+				'date' : [],
+				'sentiment' : [],
+	}
+
 	for k in data:
 		avg_sent = sum(data[k]) / float(len(data[k]))
-		avg_data[k] = avg_sent
+		avg_data['date'].append(k)
+		avg_data['sentiment'].append(avg_sent)
 
 	return avg_data
 
@@ -113,13 +139,17 @@ def plot_candlestick():
 
 	"""Test code"""
 	sent_data = load_sentiment(ticker, start_date, end_date)
-	print sent_data
+	#print sent_data
 	avg_data = process_sentiment(sent_data)
-	print avg_data
+	#print avg_data
 	"""End test code"""
 
 	df = pd.DataFrame(data)
 	df["date"] = pd.to_datetime(df["date"])
+
+	df2 = pd.DataFrame(avg_data)
+	df2["date"] = pd.to_datetime(df2["date"])
+	print df2
 
 	mids = (df.open + df.close)/2
 	spans = abs(df.close-df.open)
@@ -135,11 +165,11 @@ def plot_candlestick():
 	p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000)
 
 	p.segment(df.date, df.high, df.date, df.low, color="black", toolbar_location="left")
-	p.rect(df.date[inc], mids[inc], w, spans[inc], fill_color="#D5E1DD", line_color="black", source = ColumnDataSource(data={"open": df.open[inc], "close": df.close[inc]}))
-	p.rect(df.date[dec], mids[dec], w, spans[dec], fill_color="#F2583E", line_color="black", source = ColumnDataSource(data={"open": df.open[dec], "close": df.close[dec]}))
+	p.rect(df.date[inc], mids[inc], w, spans[inc], fill_color="#D5E1DD", line_color="black", source = ColumnDataSource(data={"open": df.open[inc], "close": df.close[inc], "sentiment": df2.sentiment}))
+	p.rect(df.date[dec], mids[dec], w, spans[dec], fill_color="#F2583E", line_color="black", source = ColumnDataSource(data={"open": df.open[dec], "close": df.close[dec], "sentiment": df2.sentiment}))
 
 	hover = p.select(dict(type=HoverTool))
-	hover.tooltips = {"open":"@open","closing":"@close"}
+	hover.tooltips = {"open":"@open","closing":"@close", "sentiment":"@sentiment"}
 
 	p.title = "{} Candlestick".format(ticker)
 	p.xaxis.major_label_orientation = pi/4
